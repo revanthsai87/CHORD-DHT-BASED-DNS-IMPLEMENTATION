7e1a2724e0d4586d93efabcad53979e17dc9b8a9
SuryaGrandhi <suryagrandhi97@gmail.com>
2021-11-24 20:14:54 +0530

    added runQueries func in driver

--- src/_in/driver/Driver.java
+++ src/_in/driver/Driver.java
@@ -31,64 +31,88 @@ public Driver(){
 		System.setProperty("de.uniba.wiai.lspi.chord.service.impl.ChordImpl.successors", "" + successor_list_size);
 		// Create BootStrap Node
 	}
+public void _IP1_Helper(_IP1.driver.DriverHelper d,String s) {
+		d.runQueries(s);
+	}
+public void _IP2_Helper(_IP2.driver.DriverHelper d,String s) {
+		d.runQueries(s);
+	}
+public void _IP3_Helper(_IP3.driver.DriverHelper d,String s) {
+	d.runQueries(s);
+}
+public void _IP4_Helper(_IP4.driver.DriverHelper d,String s) {
+	d.runQueries(s);
+}
+public void _IP5_Helper(_IP5.driver.DriverHelper d,String s) {
+	d.runQueries(s);
+}
+	public void _LEVEL3_Helper(int n,String s) {
+		if(n==1) {
+			_IP1.driver.DriverHelper helper = new _IP1.driver.DriverHelper();
+			//_IP1.driver.Driver d=new _IP1.driver.Driver();
+			//d.Initialization();
+			helper.createBootStrapNode(16);
+			helper.createNNodes(8);
+			helper.insertData();
+			_IP1_Helper(helper, s);
+		//	helper.runQueries(s);
+		}
+		else if(n==2)
+		{
+			_IP2.driver.DriverHelper help=new _IP2.driver.DriverHelper();
+		//	_in.driver.Driver d=new _in.driver.Driver();
+			//d.Initialization();
+			help.createBootStrapNode(16);
+			help.createNNodes(8);
+			help.insertData();
+			_IP2_Helper(help,s);
+			//help.runQueries(s);
+			//System.out.println("NEED TO IMPLEMENT");
+	
+	}
+		else if(n==3) {
+		_IP3.driver.DriverHelper help=new _IP3.driver.DriverHelper();
+		
+		help.createBootStrapNode(16);
+		help.createNNodes(8);
+		help.insertData();
+		_IP3_Helper(help,s);
+		
+		}
+		else if(n==4) {
+		_IP4.driver.DriverHelper help=new _IP4.driver.DriverHelper();
+		
+		help.createBootStrapNode(16);
+		help.createNNodes(8);
+		help.insertData();
+		_IP4_Helper(help,s);
+		
+		}
+		else if(n==5) {
+		_IP5.driver.DriverHelper help=new _IP5.driver.DriverHelper();
+		
+		help.createBootStrapNode(16);
+		help.createNNodes(8);
+		help.insertData();
+		_IP5_Helper(help,s);
+		
+		}
+
+		else {
+			System.out.println("NEED TO IMPLEMENT THIS AGAIN");
+		}
+
+	}
+	public void LEVEL3_HELPER() {
+		
+	}												   
 	
 	public static void main(String[] args) {
-//		Driver d=new Driver();
-//		Scanner sc=
-//		DriverHelper helper = new DriverHelper(args);
-//		
-//		helper.createBootStrapNode(d.m);
-//		// Create N nodes
-//		helper.createNNodes(d.nodes - 1);
-//		// Insert Data
-//		helper.insertData();
-//		int choice;
-//		 boolean b1=true;  
-//		while(b1){
-//			System.out.println("Option -1 : Add Node:::::::::::\n");
-//    System.out.println("Option -2 : Delete Node:::::::::\n");
-//     System.out.println("Option -3 : Query the Domain:::::::::\n");
-//	 System.out.println("Option -4 : print live nodes and deleted nodes:::::::::\n");
-//	 System.out.println("Option -5 : Shutdown and exit \n");
-//			choice=sc.nextInt();
-//			
-//			
-//			if(choice==1){
-//				helper.createAdditionalNode();
-//			}
-//			
-//			else if(choice ==2){
-//				helper.delete_activenode();
-//			}
-//			
-//			else if(choice == 3){
-//		//		helper.runQueries(queries, prob_failure, run_number);
-//			}
-//			else if(choice == 4){
-//				helper.print_livenodes();
-//			}
-//			else{
-//				helper.shutDownAllNodes(run_number);
-//		System.out.println("Process Exited");
-//		     b1=false;
-//				
-		//	}
-			
-			
 		}
 			
 			
 			
 		
 		
-		
-		
-		//Correctness Testing
-		//helper.testCorrectness();
-		// LookUp Keys
-		//helper.runQueries(queries, prob_failure, run_number);
-		// ShutDown Nodes
-		//helper.shutDownAllNodes(run_number);
-		//System.out.println("Process Exited");
-	//}
+	
 }
--- src/_net/driver/Driver.java
+++ src/_net/driver/Driver.java
@@ -55,6 +55,82 @@ public Driver(){
 		// Create BootStrap Node
 	}
 	
+	public void _IP1_Helper(_IP1.driver.DriverHelper d,String s) {
+		d.runQueries(s);
+	}
+public void _IP2_Helper(_IP2.driver.DriverHelper d,String s) {
+		d.runQueries(s);
+	}
+public void _IP3_Helper(_IP3.driver.DriverHelper d,String s) {
+	d.runQueries(s);
+}
+public void _IP4_Helper(_IP4.driver.DriverHelper d,String s) {
+	d.runQueries(s);
+}
+public void _IP5_Helper(_IP5.driver.DriverHelper d,String s) {
+	d.runQueries(s);
+}
+	public void _LEVEL3_Helper(int n,String s) {
+		if(n==1) {
+			_IP1.driver.DriverHelper helper = new _IP1.driver.DriverHelper();
+			//_IP1.driver.Driver d=new _IP1.driver.Driver();
+			//d.Initialization();
+			helper.createBootStrapNode(16);
+			helper.createNNodes(8);
+			helper.insertData();
+			_IP1_Helper(helper, s);
+		//	helper.runQueries(s);
+		}
+		else if(n==2)
+		{
+			_IP2.driver.DriverHelper help=new _IP2.driver.DriverHelper();
+		//	_in.driver.Driver d=new _in.driver.Driver();
+			//d.Initialization();
+			help.createBootStrapNode(16);
+			help.createNNodes(8);
+			help.insertData();
+			_IP2_Helper(help,s);
+			//help.runQueries(s);
+			//System.out.println("NEED TO IMPLEMENT");
+	
+	}
+		else if(n==3) {
+		_IP3.driver.DriverHelper help=new _IP3.driver.DriverHelper();
+		
+		help.createBootStrapNode(16);
+		help.createNNodes(8);
+		help.insertData();
+		_IP3_Helper(help,s);
+		
+		}
+		else if(n==4) {
+		_IP4.driver.DriverHelper help=new _IP4.driver.DriverHelper();
+		
+		help.createBootStrapNode(16);
+		help.createNNodes(8);
+		help.insertData();
+		_IP4_Helper(help,s);
+		
+		}
+		else if(n==5) {
+		_IP5.driver.DriverHelper help=new _IP5.driver.DriverHelper();
+		
+		help.createBootStrapNode(16);
+		help.createNNodes(8);
+		help.insertData();
+		_IP5_Helper(help,s);
+		
+		}
+
+		else {
+			System.out.println("NEED TO IMPLEMENT THIS AGAIN");
+		}
+
+	}
+	public void LEVEL3_HELPER() {
+		
+	}
+	
 	public static void main(String[] args) {
 //		Driver d=new Driver();
 //		Scanner sc=
