ec63681954e3de1a7158296c999ee32814366829
SuryaGrandhi <suryagrandhi97@gmail.com>
2021-11-25 14:36:45 +0530

    multiple ip entries

--- src/_IP1/driver/DriverHelper.java
+++ src/_IP1/driver/DriverHelper.java
@@ -229,7 +229,12 @@ public class DriverHelper {
 						// If value is a NS record or CName record
 						//IF ns record for now print the respective value.
 						//If cname record return the vale to root.
-						System.out.println("IP1-->TEST VALUE---- "+value);
+						Vector<String> res = splitStrings(value, '?');
+						for (String x : res)
+						{
+							System.out.println("IP1-->TEST VALUE---- "+x);
+						}
+						//System.out.println("IP1-->TEST VALUE---- "+value);
 						}
 				}
 		}
@@ -240,6 +245,51 @@ public class DriverHelper {
 		
 		return;
 	}
+	
+	static Vector<String> splitStrings(String str, char dl)
+    {
+        String word = "";
+ 
+        // to count the number of split Strings
+        int num = 0;
+ 
+        // adding delimiter character
+        // at the end of 'str'
+        str = str + dl;
+ 
+        // length of 'str'
+        int l = str.length();
+ 
+        // traversing 'str' from left to right
+        Vector<String> substr_list = new Vector<String>();
+        for (int i = 0; i < l; i++)
+        {
+ 
+            // if str[i] is not equal to the delimiter
+            // character then accumulate it to 'word'
+            if (str.charAt(i) != dl)
+            {
+                word = word + str.charAt(i);
+            }
+            else
+            {
+ 
+                // if 'word' is not an empty String,
+                // then add this 'word' to the array
+                // 'substr_list[]'
+                if ((int) word.length() != 0)
+                {
+                    substr_list.add(word);
+                }
+ 
+                // reset 'word'
+                word = "";
+            }
+        }
+ 
+        // return the splitted Strings
+        return substr_list;
+    }
 
 	/**
 	 * Method to randomly select data that will be queried.
